1a8c31c7d3a1965414d2c6ef9bbd1a206899b46a
yostane <yostane@yahoo.fr>
2017-07-09 12:55:00 +0200

    1st commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_core_tutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
